08bbe079064509ee6563ee4fbddb2ae46e40ea8b
Tsunami <U16106818@sharjah.ac.ae>
2021-11-09 22:33:11 +0400

    Updated the codes

--- EVotingTesting/DataValidationTest.cs
+++ EVotingTesting/DataValidationTest.cs
@@ -8,29 +8,56 @@ namespace EVotingTesting
         [TestMethod]
         public void EmailValidationTest()
         {
-            //bool ExpectdResult = true;
-            //bool ActualResult = DataValidation.SignInValidation(new SignInModel("User@gmail.com", "qwert123"));
-            //Assert.AreEqual(ExpectdResult, ActualResult);
+            Validation.EmailValidation emailValidator = new Validation.EmailValidation();
+
+            bool Result = emailValidator.IsValid("H12345678@hct.ac.ae");
+
+            //The result value should be true in case the test to be passed.
+            Assert.IsTrue(Result);
         }
 
         [TestMethod]
         public void StudentIdValidationTest()
         {
+            Validation.StudentIdValidation studentIdValidator = new Validation.StudentIdValidation();
+
+            bool Result = studentIdValidator.IsValid("H12345678");
+
+            //The result value should be true in case the test to be passed.
+            Assert.IsTrue(Result);
         }
 
         [TestMethod]
         public void NameValidationTest()
         {
+            Validation.NameValidation nameValidator = new Validation.NameValidation();
+
+            bool Result = nameValidator.IsValid("Aisha");
+
+            //The result value should be true in case the test to be passed.
+            Assert.IsTrue(Result);
         }
 
         [TestMethod]
         public void NationalIdValidationTest()
         {
+            Validation.NationalIdValidation nationalIdValidator = new Validation.NationalIdValidation();
+
+            bool Result = nationalIdValidator.IsValid("000-1111-0000111-1");
+
+            //The result value should be true in case the test to be passed.
+            Assert.IsTrue(Result);
         }
 
         [TestMethod]
         public void GenderValidationTest()
         {
+            Validation.GenderValidation genderValidator = new Validation.GenderValidation();
+
+            bool Result = genderValidator.IsValid("Female");
+
+            //The result value should be true in case the test to be passed.
+            Assert.IsTrue(Result);
         }
     }
 }
